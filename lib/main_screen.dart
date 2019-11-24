e65304be6f4db38b397cc89f2be084d5c9df7f51
primeLasitha <prime.lasitha@gmail.com>
2019-11-24 08:31:30 +0530

    created simple location screen

---
+++ lib/main_screen.dart
@@ -0,0 +1,9 @@
+import 'package:bloc/location.dart';
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return LocationScreen();
+  }
+}
